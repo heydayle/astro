807065ecf890ca37a8a6edfa1788012a945063f8
heyday <hungthinh.ckc@gmail.com>
2025-01-12 15:37:04 +0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -10,4 +9,11 @@ export default defineConfig({
   integrations: [vue(), react({ include: ['**/react/*', '**/shared/*'] }), tailwind({
     applyBaseStyles: false,
   })],
+  vite: {
+    resolve: {
+      alias: {
+        '@': 'src'
+      },
+    },
+  },
 });
